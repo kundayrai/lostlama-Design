87610205ef2b0803f2f0ac1990cdc0d88417441e
Kunal Prakash Rai <kunal.p.rai11@gmail.com>
2017-12-12 16:57:42 +0530

    Update #121217

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,16 @@
-jQuery(document).ready(function($){
-
-jQuery('.who-we-are-slider').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        speed: 300,
-        slide: 'div',
-        cssEase: 'linear',
-        prevArrow:'<div class="slick-prev-button"><i class="fa  fa-chevron-circle-left fa-2x" style="color:#FF5A5F"></i></div>',
-        nextArrow:'<div class="slick-next-button"><i class="fa  fa-chevron-circle-right fa-2x" style="color:#FF5A5F"></i></div>',
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-
-                }
-            },
-            {
-                breakpoint: 960,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerMode:true
-
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-
-
+$(document).ready(function(){
+var scroll_start = 0;
+var startchange = $('#navbar');
+var offset = startchange.offset();
+if (startchange.length){
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+        $(".fixed").addClass("colored-nav");
+        } 
+        else {
+        $('.fixed').removeClass("colored-nav");
+    }
     });
-
-    jQuery('.who-we-are-slider').css('visibility', 'visible');
+}
 });
-
